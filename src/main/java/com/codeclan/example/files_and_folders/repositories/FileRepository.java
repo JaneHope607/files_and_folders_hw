1814f75641d8317891027ea2adbce1548b6f8ab3
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-09-08 17:03:20 +0100

    completed file, folder and user repository

--- src/main/java/com/codeclan/example/files_and_folders/repositories/FileRepository.java
+++ src/main/java/com/codeclan/example/files_and_folders/repositories/FileRepository.java
@@ -1,4 +1,9 @@
 package com.codeclan.example.files_and_folders.repositories;
 
-public interface FileRepository {
+import com.codeclan.example.files_and_folders.models.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileRepository extends JpaRepository<File, Long> {
 }
--- src/main/java/com/codeclan/example/files_and_folders/repositories/UserRepository.java
+++ src/main/java/com/codeclan/example/files_and_folders/repositories/UserRepository.java
@@ -1,4 +1,9 @@
 package com.codeclan.example.files_and_folders.repositories;
 
-public interface UserRepository {
+import com.codeclan.example.files_and_folders.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
